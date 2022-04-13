b37a92944b10ec867b77c7573b257dd2d92f6c0a
shankysai <61941953+shankysai@users.noreply.github.com>
2022-04-13 09:44:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,7 @@ import subprocess
 
 # From Python3.7 you can add
 # keyword argument capture_output
-print(subprocess.run(["echo", "Geeks for geeks"],
-					capture_output=True))
+#print(subprocess.run(["echo", "Geeks for geeks"],capture_output=True))
 
 # For older versions of Python:
-#print(subprocess.check_output(["echo","Geeks for geeks"]))
+print(subprocess.check_output(["echo","Geeks for geeks"]))
