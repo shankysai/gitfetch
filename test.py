e49969a1b81306b8d648cd4ca56bd043a094d1ce
shankysai <61941953+shankysai@users.noreply.github.com>
2022-04-13 09:41:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,4 @@ print(subprocess.run(["echo", "Geeks for geeks"],
 					capture_output=True))
 
 # For older versions of Python:
-#print(subprocess.check_output(["echo",
-							"Geeks for geeks"]))
-
+#print(subprocess.check_output(["echo","Geeks for geeks"]))
