772d63ab2ae43a7ab137001ef8a2af5751302588
shankysai <61941953+shankysai@users.noreply.github.com>
2022-04-15 00:12:24 +0530

    Create mailtest.sh

---
+++ mailtest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Checking Mail Server #1"
+echo "QUIT" | nc -w 5 smtp.gmail.com > /dev/null 2>&1
+if [ $? == 0 ]; then
+  echo "mail server #1 is UP"
+else
+  echo "mail server #1 is DOWN"
+fi
