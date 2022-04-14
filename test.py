cebf9b9c066a3ba7c6bdb8921a9ee8ad4c008b37
shankysai <61941953+shankysai@users.noreply.github.com>
2022-04-15 00:10:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,5 @@ import subprocess
 #print(subprocess.run(["echo", "Geeks for geeks"],capture_output=True))
 
 # For older versions of Python:
-print(subprocess.check_output([ "timeout", "-s", "KILL", "1m", "date" ]))
+#print(subprocess.check_output([ "timeout", "-s", "KILL", "1m", "date" ]))
+print("Hello world")
